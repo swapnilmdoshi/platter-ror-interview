9736801fa5d502e31a20c068c2d62668db782937
Swapnil Doshi <v-sdoshi@swiftly.com>
2025-08-07 17:15:59 +0530

    bare minimum working billing service.

---
+++ price_calculator.rb
@@ -0,0 +1,70 @@
+require 'json'
+require 'pry'
+
+class PriceCalculator
+  def initialize
+    file_content = File.read('./commodity_master.json')
+    @commodity_master = JSON.parse(file_content)        
+    @item_count = {}
+    @bill = Hash.new {|attribute,value| attribute[value] = Hash.new(0)}
+    @total_price = nil
+  end
+
+  def get_user_input
+    puts 'Please enter all the items purchased separated by a comma'
+    @input_string = gets    
+  end
+  
+  def fetch_items  
+    @purchased_items = @input_string.split(",").map(&:strip)    
+  end
+
+  def count_items    
+    @item_count = @purchased_items.tally
+  end
+
+  def calculate_total
+    @item_count
+
+    @item_count.each do |item, item_quantity|
+      valid_commodity = @commodity_master.fetch(item, nil)
+      unit_price = valid_commodity.fetch('unit_price',nil)
+      offer = valid_commodity.fetch('offer', nil)
+
+      offer_min_quantity = nil
+      offer_price = nil
+
+      if offer
+        offer_min_quantity, offer_price = offer.values_at('minimum_quantity', 'price')
+      end
+
+      if offer && item_quantity >= offer_min_quantity
+        applicable_quantity = offer_min_quantity
+        remaining_quantity = item_quantity - offer_min_quantity
+                  
+        @bill[item]['quantity'] = item_quantity
+        @bill[item]['total_price'] = offer_price + (remaining_quantity * unit_price)
+      else
+        @bill[item]['quantity'] = item_quantity
+        @bill[item]['total_price'] = unit_price * item_quantity
+      end
+    end      
+  end
+ 
+  def print_bill
+    puts("Items".ljust(25) + "Quantity".ljust(15) + "Price".ljust(15))
+    puts "_" * 55
+
+    @bill.each do| item_name, details|
+      puts  item_name.ljust(25) + details['quantity'].to_s.ljust(15) + details['total_price'].to_s.ljust(15)
+    end
+  end
+end
+
+calculator = PriceCalculator.new
+
+calculator.get_user_input
+calculator.fetch_items
+calculator.count_items
+calculator.calculate_total
+calculator.print_bill
